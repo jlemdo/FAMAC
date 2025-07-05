1cfead08ea32236c67690f94bc935dbec3ca0e57
jlemdo <jlelpella@gmail.com>
2025-07-04 23:51:25 -0600

    firts commit

---
+++ ios/MyNewApp/main.m
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+    // NSStringName de tu AppDelegate
+    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
