3946ef51ca61971e99917cce090c6a6e0e5f9e28
jlemdo <jlelpella@gmail.com>
2025-07-05 15:43:06 -0600

    Cambio a Pod file editando con xcode

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,23 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+// metro.config.js
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const os = require('os');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+// En Node <18 no existe os.availableParallelism(), así que caemos a os.cpus().length
+const availableWorkers = typeof os.availableParallelism === 'function'
+  ? os.availableParallelism()
+  : os.cpus().length;
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(
+  getDefaultConfig(__dirname),
+  {
+    // Inyectamos maxWorkers en la configuración final
+    maxWorkers: availableWorkers,
+    transformer: {
+      // tus otras opciones de transformer, si las tuvieras...
+    },
+    resolver: {
+      // tus otras opciones de resolver, si las tuvieras...
+    },
+    // etc.
+  }
+);
