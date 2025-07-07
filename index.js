309d564d69c0d00808bb6cc216b6c0c33e96fc11
jlemdo <jlelpella@gmail.com>
2025-07-07 11:46:51 -0600

    Cambio a Pod file editando con xcode

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-/**
- * @format
- */
+// index.js
+import { AppRegistry } from 'react-native';
+import React from 'react';
+import { View, Text } from 'react-native';
+import { name as appName } from './app.json';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => () => (
+  <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+    <Text>🚀 Hello World</Text>
+  </View>
+));
